0277700b6d9d4e4a6342a6f213229977927df511
Mark <markmccoid@gmail.com>
2025-06-10 08:20:26 -0400

    version bump

--- app.config.js
+++ app.config.js
@@ -13,7 +13,7 @@ export default {
   scheme: "littleapeaudio",
   privacy: "unlisted",
   platforms: ["ios"],
-  version: "1.1.92",
+  version: "1.1.94",
   orientation: "portrait",
   icon: "./assets/icon.png",
   userInterfaceStyle: "automatic",
@@ -35,7 +35,7 @@ export default {
     },
     supportsTablet: false,
     bundleIdentifier: "com.markmccoid.little-ape-audio",
-    buildNumber: "1.1.92",
+    buildNumber: "1.1.94",
     infoPlist: {
       RCTAsyncStorageExcludeFromBackup: false,
       NSMicrophoneUsageDescription:
